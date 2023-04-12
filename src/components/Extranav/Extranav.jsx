cca20ba89dc53181887a8237396a59f05b6898d9
iceddrop <93996532+iceddrop@users.noreply.github.com>
2023-04-12 21:52:43 +0100

    extranav progress 2

--- src/components/Extranav/Extranav.jsx
+++ src/components/Extranav/Extranav.jsx
@@ -2,25 +2,33 @@ import React from "react";
 import './Extranav.css';
 import {HiOutlinePhone} from "react-icons/hi";
 import {FiChevronDown} from "react-icons/fi";
+import "../../index.css";
+
 export default function Extranav(){
     return(
        <section className="extra-nav">
           <div className="contact-div">
-             <HiOutlinePhone/>
-             <p>+001234567890</p>
+             <HiOutlinePhone className="phone-icon"/>
+             <p className="phone-number">+001234567890</p>
           </div>
           <div className="discount-div">
-             <p>jdhjsh</p>
-             <p>dyujyduj</p>
+             <p>Get 50% Off on Selected Items </p>
+             <p>|</p>
+             <p>Shop Now</p>
           </div>
           <div className="dropdown-div">
              <div className="dropdown">
-                <p>Eng</p>
-                <FiChevronDown/>
+                <p className="dropdown-text">Eng</p>
+                <FiChevronDown className="chevron-down"/>
+                <ul className="dropdown-list">
+                  <li className=""></li>
+                  <li className=""></li>
+                  <li className=""></li>
+                </ul>
              </div>
              <div className="dropdown">
                  <p>Location</p>
-                 <FiChevronDown/>
+                 <FiChevronDown className="chevron-down"/>
              </div>
           </div>
        </section>
